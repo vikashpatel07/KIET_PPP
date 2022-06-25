ecc216a08e3ab9518691ab25993511e13feab949
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-06-25 13:21:08 +0530

    Time: 2 ms (30.74%), Space: 54.7 MB (8.60%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean checkPossibility(int[] nums) {
+        int count=0;
+        for(int i=0;i<nums.length-1;i++){
+            if(nums[i+1]<nums[i]){
+               count++;
+                if(count>1)
+                    return false;
+                if(i-1<0 || nums[i-1]<=nums[i+1])
+                    nums[i]=nums[i+1];
+                else
+                    nums[i+1]=nums[i];
+            }
+        }
+        if(count<=1)return true;
+        else return false;
+        
+    }
+}
