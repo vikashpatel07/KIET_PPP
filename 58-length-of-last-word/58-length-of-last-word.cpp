222e06de7fa272cb6552b51b189ab2c6adaacbf4
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-06-29 18:36:27 +0530

    Time: 3 ms (56.36%), Space: 6.8 MB (7.56%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        while(ss>>word){
+            // gulabo jara ittar gira to;;;;
+            
+            // mere sapno ki raani kb aayegi tu...aai rut mastaani badi aayegi tu,,,,bit jaaye zindagaani kb aayegi tu....chali aa tu chali aa......
+            // ye duniya ye mahfil mere kaam ki nahi
+            // mai roya tujhe dhundh dhundh ke roya 
+            // ek baat kahu kya izazat hai tere ishq ki mujhko aadat hai
+            // kaise bataye kaise zataye subah tk tujhame zina chahe bheege lamho ki
+            
+        }
+        return word.size();
+    }
+};
