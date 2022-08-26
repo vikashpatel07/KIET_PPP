eb15544d55221d721365a65817808606ea3673ea
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-08-26 16:32:48 +0530

    Time: 511 ms (6.78%), Space: 6.1 MB (68.93%) - LeetHub

--- 869-reordered-power-of-2/869-reordered-power-of-2.cpp
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
-    string solve(int n){
-        string res=to_string(n);
-        sort(res.begin(),res.end());
-        return res;
-        
-    }
-    bool reorderedPowerOf2(int n) {
-        string t=solve(n);
-        for(int i=0;i<31;i++){
-            if(t==solve(1<<i))return true;
+    bool check(vector<int>&a){
+        if(a[0]==0)return false;
+        int  num=0;
+        int m=a.size();
+        for(int i=0;i<m;i++){
+            num=10*num+a[i];
+        }
+        while(num>0 && (num&1)==0)
+            num>>=1;
+        if(num==1)return true;
+        return false;
+        }
+    bool permutation(vector<int> &a,int start){
+        if(start==a.size())return check(a);
+        for(int i=start;i<a.size();i++){
+            swap(a[start],a[i]);
+            if(permutation(a,start+1))
+                return true;
+            swap(a[start],a[i]);
         }
         return false;
+    }
+    bool reorderedPowerOf2(int n) {
+        string temp=to_string(n);
+        int m=temp.size();
+        vector<int>a(m);
+        for(int i=0;i<m;i++){
+            a[i]=temp[i]-'0';
+        }
+        return permutation(a,0);
+        
     }
 };
