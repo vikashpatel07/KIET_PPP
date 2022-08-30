7edf065e04cb0eaa90abef5c24488a49aeb07208
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-08-30 12:21:38 +0530

    Time: 7 ms (66.07%), Space: 11.2 MB (34.37%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
+        int n=mat.size();
+        int k=4;
+        while(k--){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                swap(mat[i][j],mat[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++)reverse(mat[i].begin(),mat[i].end());
+        if(mat==target)return true;
+    }
+    return false;
+    }
+};
