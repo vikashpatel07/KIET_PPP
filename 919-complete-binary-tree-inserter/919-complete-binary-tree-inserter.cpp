cbdcf018e76cfd6298ce60cf9307f054a0cb3dfc
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-06 11:08:55 +0530

    Time: 106 ms (20.91%), Space: 64.3 MB (11.27%) - LeetHub

--- 919-complete-binary-tree-inserter/919-complete-binary-tree-inserter.cpp
+++ 919-complete-binary-tree-inserter/919-complete-binary-tree-inserter.cpp
@@ -14,47 +14,24 @@ public:
     TreeNode* node;
     CBTInserter(TreeNode* root) {
         node=root;
-        
     }
-
+    
     int insert(int val) {
         queue<TreeNode*>q;
+        
         q.push(node);
-        int k=0,cnt=0;
-        vector<TreeNode*>temp;
         while(!q.empty()){
-            int n=q.size();
-            
-            int z=(int)(pow(2,k));
-            
-            if(z==n){
-                k++;
-                temp.clear();
-                for(int i=0;i<n;i++){
-                    auto x=q.front();
-                    temp.push_back(x);
-                    q.pop();
-                    if(x->left)q.push(x->left);
-                    if(x->right)q.push(x->right);
-                }
-            }
+            auto x=q.front();
+            q.pop();
+            if(x->left)q.push(x->left);
             else{
-                for(int j=0;j<temp.size();j++){
-                    if(!temp[j]->left){
-                        temp[j]->left=new TreeNode(val);
-                        return temp[j]->val;
-                    }
-                    else if(!temp[j]->right){
-                        temp[j]->right=new TreeNode(val);
-                    return temp[j]->val;
-                }
-                }
+                x->left=new TreeNode (val);
+                return x->val;
             }
-            
-            
-            if(q.size()==0){
-                 temp[0]->left=new TreeNode(val);
-                return temp[0]->val;
+            if(x->right)q.push(x->right);
+            else{
+                x->right=new TreeNode (val);
+                return x->val;
             }
         }
         return 0;
