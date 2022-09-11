253859c6a83756d2b7576fafdeb3ecfd936e886b
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-11 21:50:35 +0530

    Time: 743 ms (21.63%), Space: 53.3 MB (18.09%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    vector<vector<int>>dir={{-1,0},{0,-1},{0,1},{1,0}};
-    void dfs(int i,int j, vector<vector<int>> &vis,vector<vector<int>> &h,int mid){
+    int minimumEffortPath(vector<vector<int>>& h) {
         int n=h.size();
         int m=h[0].size();
-        if(!vis[i][j]){
-            vis[i][j]=1;
-           for(int d=0;d<4;d++){
-               int x=i+dir[d][0];
-               int y=j+dir[d][1];
-               if(x>=0 && x<n && y>=0 && y<m && abs(h[i][j]-h[x][y])<=mid)
-                   dfs(x,y,vis,h,mid);
-           }
-        }
-    }
-    int minimumEffortPath(vector<vector<int>>& h) {
-        int n=h.size(),m=h[0].size();
-        int start=0,end=1e9,ans=0;
-        while(start<=end){
-            int mid=start+(end-start)/2;
-            vector<vector<int>> vis(n,vector<int>(m,0));
-            dfs(0,0,vis,h,mid);
-            if(vis[n-1][m-1]){
-                ans=mid;
-                end=mid-1;
+        using pi=pair<int,pair<int,int>>;
+        int ans=0;
+        priority_queue<pi,vector<pi>,greater<pi>>pq;
+        pq.push({0,{0,0}});
+        vector<vector<int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
+        while(!pq.empty()){
+            int wt=pq.top().first;
+            int u=pq.top().second.first;
+            int v=pq.top().second.second;
+            pq.pop();
+            ans=max(ans,wt);
+            if(u==n-1 && v==m-1)return ans;
+            
+            if(h[u][v]==-1)continue;
+            int ht=h[u][v];
+            h[u][v]=-1;
+            for(auto x: dir){
+                int dx=u+x[0];
+                int dy=v+x[1];
+                if(dx>=0 && dx<n && dy>=0 && dy<m)
+                    pq.push({abs(h[dx][dy]-ht),{dx,dy}});
             }
-            else start=mid+1;
         }
-        return ans;
+        return 0;
+        
     }
 };
