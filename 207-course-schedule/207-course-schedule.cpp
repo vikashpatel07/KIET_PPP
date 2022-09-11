4733bdba20030773f0e859573b6e11f3cf99c6df
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-11 11:55:27 +0530

    Time: 15 ms (99.19%), Space: 13.2 MB (88.72%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool canFinish(int n, vector<vector<int>>& p) {
+       vector<int>adj[n];
+        for(int i=0;i<p.size();i++){
+            // adj[p[i][0]].push_back(p[i][1]);
+            adj[p[i][1]].push_back(p[i][0]);
+        }
+        vector<int>ind(n,0);
+        for(int i=0;i<n;i++){
+            for(auto x: adj[i]){
+                ind[x]++;
+            }
+        }
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            if(ind[i]==0)q.push(i);
+        }
+        vector<int>vis(n,0);
+        vector<int>res;
+        while(!q.empty()){
+            auto x=q.front();
+            q.pop();
+            res.push_back(x);
+          
+            for(auto it: adj[x]){
+                ind[it]--;
+                if(ind[it]==0)q.push(it);
+            }
+            
+        }
+        return res.size()==n;
+    }
+};
