70deefb3b98719749cd81845f8927fde164359af
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-11 12:50:48 +0530

    Time: 472 ms (10.24%), Space: 59.2 MB (26.07%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,31 +1,54 @@
 class Solution {
 public:
+    bool dfs(int node,vector<bool>  &vis,vector<bool> & safe,vector<int> adj[],vector<bool> &cur)
+    {
+        vis[node]=true;
+       cur[node]=true;
+       
+       for(auto x: adj[node])
+       {
+           if(!vis[x])
+           {
+               if(dfs(x,vis,safe,adj,cur)==false)
+                   return safe[node]=false;
+           }
+           else if(cur[x]==true)
+               return safe[node]=false;
+       }
+        cur[node]=false;
+        // return safe[node];
+        return true;
+    }
+      
+        
+    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n=graph.size();
         vector<int>adj[n];
-        vector<int>ind(n,0);
-        for(int i=0;i<n;i++){
-            for(auto x: graph[i]){
-                adj[x].push_back(i);
-                ind[i]++;
-            }
+         unordered_map<int,int>mp;
+        for(int i=0;i<n;i++)
+        {
+           
+            for(auto x: graph[i])
+            adj[i].push_back(x);
         }
-        queue<int>q;
         vector<int>res;
+        vector<bool>vis(n,false);
+        vector<bool>cur(n,false);
+        vector<bool>safe(n,true);
+        for(int i=0;i<n;i++)
+        {
+          if (!vis[i])
+                dfs(i,vis,safe,adj,cur);
+           
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(safe[i])
+                res.push_back(i);
+        }
         
-        for(int i=0;i<n;i++){
-            if(ind[i]==0)q.push(i);
-        }
-        while(!q.empty()){
-            auto temp=q.front();
-            q.pop();
-            res.push_back(temp);
-            for(auto x: adj[temp]){
-                ind[x]--;
-                if(ind[x]==0)q.push(x);
-            }
-        }
-        sort(res.begin(),res.end());
         return res;
+        
     }
 };
