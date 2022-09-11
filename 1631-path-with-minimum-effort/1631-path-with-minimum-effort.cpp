2c4ccf8ee58aaabecfe08589fe95b325a469d885
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-11 17:38:06 +0530

    Time: 1276 ms (7.83%), Space: 53.8 MB (17.97%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-     int vis[105][105];
-    vector<pair<int,int>>dir={{1,0},{-1,0},{0,-1},{0,1}};
-    void ok(int x, int y,int mid , vector<vector<int>>& heights){
-        if(!vis[x][y]){
-            vis[x][y]=1;
-            int n=heights.size();
-            int m=heights[0].size();
-            for(int i=0;i<4;i++){
-                int X=x+dir[i].first;
-                int Y=y+dir[i].second;
-                if(X<0 || X>=n || Y<0 || Y>=m)
-                    continue;
-                if(abs(heights[x][y]-heights[X][Y])<=mid)
-                    ok(X,Y,mid,heights);
-            }
+    vector<pair<int,int>>dir={{-1,0},{0,-1},{0,1},{1,0}};
+    void dfs(int i,int j, vector<vector<int>> &vis,vector<vector<int>> &h,int mid){
+        int n=h.size();
+        int m=h[0].size();
+        if(!vis[i][j]){
+            vis[i][j]=1;
+           for(int d=0;d<4;d++){
+               int x=i+dir[d].first;
+               int y=j+dir[d].second;
+               if(x<0 || x>=n || y<0 ||y>=m)continue;
+               if(abs(h[i][j]-h[x][y])<=mid)
+                   dfs(x,y,vis,h,mid);
+           }
         }
     }
-    
-    int minimumEffortPath(vector<vector<int>>& heights) {
-        int l=0,r=1e9+2;
-        int n=heights.size();
-        int m=heights[0].size();
-        while(l<r){
-            int mid=(l+r)/2;
-            memset(vis,0,sizeof(vis));
-            ok(0,0,mid,heights);
-            if(vis[n-1][m-1]==1)
-                r=mid;
-            else
-                l=mid+1;
+    int minimumEffortPath(vector<vector<int>>& h) {
+        int n=h.size(),m=h[0].size();
+        int start=0,end=1e9,ans=0;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            vector<vector<int>> vis(n,vector<int>(m,0));
+            dfs(0,0,vis,h,mid);
+            if(vis[n-1][m-1]){
+                ans=mid;
+                end=mid-1;
+            }
+            else start=mid+1;
         }
-        return l;
+        return ans;
     }
 };
