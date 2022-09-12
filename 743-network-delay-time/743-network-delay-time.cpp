c2640ad9daa9fca197c798269d30a74ccbc6070b
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-12 10:23:26 +0530

    Time: 2903 ms (5.01%), Space: 274.9 MB (5.04%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,34 +1,27 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& t, int n, int k) {
-        vector<pair<int,int>>adj[n+1];
-        for(int i=0;i<t.size();i++){
-            adj[t[i][0]].push_back({t[i][1],t[i][2]});
-        }
-        vector<int>vis(n+1,0);
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         using pi=pair<int,int>;
-        priority_queue<pi,vector<pi>,greater<pi>>pq;
-        pq.push({0,k});
-        int cost=0;
-       int res=0;
-        while(!pq.empty()){
-            int wt=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            vis[node]=1;
-           res=max(res,wt);
-            int cnt=0;
-            for(int i=1;i<n+1;i++){
-                if(vis[i])cnt++;
-            }
-            if(cnt==n)return res;
-            for(auto x: adj[node]){
-                if(!vis[x.first])
-                pq.push({wt+x.second,x.first});
-            }
-            
-            
+        vector<pi> adj[n+1];
+        for(int i=0;i<times.size();i++){
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
         }
-        return -1;
+        vector<int>dis(n+1,INT_MAX);
+        dis[k]=0;
+        for(int i=0;i<n-1;i++){
+            for(auto node: times){
+                int u=node[0];
+                int v=node[1];
+                int wt=node[2];
+                if(dis[u]!=INT_MAX && dis[u]+wt<dis[v])
+                    dis[v]=dis[u]+wt;
+            }
+        }
+        int res=0;
+        for(int i=1;i<=n;i++){
+            if(dis[i]==INT_MAX)return -1;
+            res=max(res,dis[i]);
+        }
+        return res;
     }
 };
