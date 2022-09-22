7781ffa768aaeab2617f1a30f328b207d77750f6
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-22 11:04:55 +0530

    Time: 59 ms (25.65%), Space: 8.3 MB (31.16%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        unordered_map<char,int>mp;
-        for(auto x: t)mp[x]++;
-        int res=INT_MAX,ind=0,i=0,j=0,count=0;
+        unordered_map<char,int>A,B;
+        for(auto x: t)B[x]++;
+        string ans="";
+        int res=INT_MAX,i=0,j=0,count=0;
         while(j<s.size()){
-            if(mp[s[j]]>0)count++;
-            mp[s[j]]--;
-            if(count==t.length()){
-                while(i<j && mp[s[i]]<0){
-                    mp[s[i]]++;
+            char ch=s[j];
+            if(B.find(ch)!=B.end()){
+                A[ch]++;
+                if(A[ch]<=B[ch])
+                    count++;
+            }
+            if(count>=t.length()){
+                while(B.find(s[i])==B.end()  || A[s[i]]>B[s[i]]){
+                    A[s[i]]--;
                     i++;
                 }
                 if(res>j-i+1){
+                    ans=s.substr(i,j-i+1);
                     res=j-i+1;
-                    ind=i;
                 }
-                mp[s[i++]]++;
-               
-                count--;
-                cout<<ind<<" ";
+                // i++;
             }
             j++;
         }
-         return res == INT_MAX ? "" : s.substr(ind, res);
+        return ans;
     }
 };
