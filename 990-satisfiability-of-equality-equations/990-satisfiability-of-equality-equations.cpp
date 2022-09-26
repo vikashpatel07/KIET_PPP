aacff5b3acc61b9a70ccd6270faefc1493fe086f
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-26 10:53:32 +0530

    Time: 19 ms (16.42%), Space: 11.2 MB (98.15%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-      vector<int> parent;
-    int findParent(int i){
-        if(parent[i] == -1) return i;
-        return parent[i] = findParent(parent[i]);
+    vector<int>parent;
+    int findpar(int i){
+        if(parent[i]==i)return i;
+        return parent[i]=findpar(parent[i]);
     }
     
-    void unionNode(int a, int b){
-        int p1 = findParent(a);
-        int p2 = findParent(b);
-        if(p1 != p2) parent[p1] = p2;
+    void unionnode(int a,int b){
+        int p1=findpar(a);
+        int p2=findpar(b);
+        if(p1!=p2)parent[p1]=p2;
     }
     bool equationsPossible(vector<string>& equations) {
-        parent.resize(26, -1);
-        for(auto e:equations){
-            if(e[1] == '=') unionNode(e[0]-'a', e[3]-'a');
+        parent.resize(26);
+        for(int i=0;i<26;i++){
+            parent[i]=i;
         }
-        for(auto e:equations){
-            if(e[1] == '!' && findParent(e[0]-'a') == findParent(e[3]-'a') ) return false;
+        
+        for(auto e: equations){
+            if(e[1]=='=')unionnode(e[0]-'a',e[3]-'a');
+        }
+        for(auto e: equations){
+            if(e[1]=='!' && (findpar(e[0]-'a')==findpar(e[3]-'a')))
+                return false;
         }
         return true;
     }
