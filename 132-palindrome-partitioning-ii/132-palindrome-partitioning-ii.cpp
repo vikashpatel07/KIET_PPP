3f83647250a9af4a48da5d45ecbc8198ca600703
Vikash Kumar Patel <72515535+vikashpatel07@users.noreply.github.com>
2022-09-26 12:06:05 +0530

    Time: 503 ms (48.72%), Space: 6.5 MB (77.38%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -24,8 +24,21 @@ public:
     }
     int minCut(string s) {
          int n=s.size();
-        vector<int>dp(n,-1);
+        vector<int>dp(n+1,0);
        
-        return solve(0,n,s,dp)-1;
+        // return solve(0,n,s,dp)-1;
+        
+        for(int i=n-1;i>=0;i--){
+              int cost=0,mini=1e9;
+            for(int j=i;j<n;j++){
+                 if(ispalindrome(i,j,s)){
+                     cost=1+dp[j+1];
+                     mini=min(mini,cost);
+            }
+            
+        }
+            dp[i]=mini;
+        }
+        return dp[0]-1;
     }
 };
